f3f9886871f1c555d14b305b5a9ff2eba01cca59
root <root@ip-172-31-42-199.ec2.internal>
2019-06-25 04:08:48 +0000

    add new files to the new terraform configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
+resource "aws_instance" "my-test-terrafomec2" {
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
 
+  tags {
+    Name = "test-instance"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+# AWS Config
 
+variable "aws_access_key" {
+  default = "AKIA3G5LNCH7SKTRXLO7"
+}
+
+variable "aws_secret_key" {
+  default = "Ov1Ax6Flj+lsLTZnjUsC+NIKR9ZzlYJ6AaG/CVCX"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
